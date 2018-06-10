019e6f6bfb4fe99f01818be40e006ed3c0009b95
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-06-10 17:07:45 +0200

    #2 Result Grid with all UI Models

---
+++ ResourceChecker/Controls/ResultControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using ResourceChecker.Models;
+
+namespace ResourceChecker.Controls
+{
+    public partial class ResultControl : DevExpress.XtraEditors.XtraUserControl
+    {
+        public ResultControl()
+        {
+            InitializeComponent();
+        }
+
+
+        public void LoadResults(List<ResultModel> resultModels, bool clear = true)
+        {
+            if (clear) resultModelBindingSource.Clear();
+            resultModels.ForEach(model =>
+                resultModelBindingSource.Add(model));
+        }
+    }
+}
